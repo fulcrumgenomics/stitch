847464f98e49b9edec8f20d3c4e529a95626e180
Nils Homer <nilshomer@gmail.com>
2023-05-31 13:48:57 -0700

    order

--- src/lib/opts.rs
+++ src/lib/opts.rs
@@ -30,23 +30,23 @@ pub struct Opts {
     pub double_strand: bool,
 
     /// The number of threads to use.
-    #[clap(long, short = 't', default_value = "2", display_order = 3)]
+    #[clap(long, short = 't', default_value = "2", display_order = 4)]
     pub threads: usize,
 
     /// Assume an unrecognized input (based on file extension) is GZIP compressed.
-    #[clap(long, short = 'z', default_value = "false", display_order = 4)]
+    #[clap(long, short = 'z', default_value = "false", display_order = 5)]
     pub decompress: bool,
 
     /// Pre-align with banded local alignment.
-    #[clap(long, short = 'p', default_value = "false", display_order = 5)]
+    #[clap(long, short = 'p', default_value = "false", display_order = 6)]
     pub pre_align: bool,
 
     /// K-mer size for banded pre-alignment.
-    #[clap(long, short = 'k', default_value = "12", display_order = 6)]
+    #[clap(long, short = 'k', default_value = "12", display_order = 7)]
     pub k: usize,
 
     /// Band size for banded pre-alignment.
-    #[clap(long, short = 'w', default_value = "50", display_order = 7)]
+    #[clap(long, short = 'w', default_value = "50", display_order = 8)]
     pub w: usize,
 
     /// Pre-align with banded local alignment.
@@ -55,20 +55,20 @@ pub struct Opts {
         short = 's',
         default_value = "100",
         allow_hyphen_values = true,
-        display_order = 8
+        display_order = 9
     )]
     pub pre_align_min_score: i32,
 
     /// Use soft-clipping for all alignments, otherwise secondary alignemnts will use hard-clipping
-    #[clap(long, short = 'S', default_value = "false", display_order = 9)]
+    #[clap(long, short = 'S', default_value = "false", display_order = 10)]
     pub soft_clip: bool,
 
     /// Use =/X CIGAR operators, otherwise use M
-    #[clap(long, short = 'X', default_value = "false", display_order = 10)]
+    #[clap(long, short = 'X', default_value = "false", display_order = 11)]
     pub use_eq_and_x: bool,
 
     /// Score for a sequence match (must be positive)
-    #[clap(long, short = 'A', default_value = "1", display_order = 11)]
+    #[clap(long, short = 'A', default_value = "1", display_order = 12)]
     pub match_score: i32,
 
     /// Score for a sequence mismatch (must be negative)
@@ -77,7 +77,7 @@ pub struct Opts {
         short = 'B',
         default_value = "-1",
         allow_hyphen_values = true,
-        display_order = 12
+        display_order = 13
     )]
     pub mismatch_score: i32,
 
@@ -87,7 +87,7 @@ pub struct Opts {
         short = 'O',
         default_value = "-5",
         allow_hyphen_values = true,
-        display_order = 13
+        display_order = 14
     )]
     pub gap_open: i32,
 
@@ -97,7 +97,7 @@ pub struct Opts {
         short = 'E',
         default_value = "-1",
         allow_hyphen_values = true,
-        display_order = 14
+        display_order = 15
     )]
     pub gap_extend: i32,
 
@@ -107,7 +107,7 @@ pub struct Opts {
         short = 'J',
         default_value = "-10",
         allow_hyphen_values = true,
-        display_order = 15
+        display_order = 16
     )]
     pub jump_score: i32,
 
@@ -115,14 +115,14 @@ pub struct Opts {
     /// - Local: aligns a sub-sequence of the read versus a sub-sequence of the reference
     /// - Semiglobal: aligns the full read versus a sub-sequence of the reference
     /// - Global: aligns the full read versus the full reference
-    #[clap(long, short = 'm', default_value = "local", display_order = 16)]
+    #[clap(long, short = 'm', default_value = "local", display_order = 17)]
     pub mode: AlignmentMode,
 
-    #[clap(long, short = 'P', default_value = "query-length", display_order = 17)]
+    #[clap(long, short = 'P', default_value = "query-length", display_order = 18)]
     pub by_primary: PrimaryPickingStrategy,
 
     /// The compression level of the output BAM
-    #[clap(long, short = 'c', default_value = "0", display_order = 18)]
+    #[clap(long, short = 'c', default_value = "0", display_order = 19)]
     pub compression: u8,
 }
 
